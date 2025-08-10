221e76eff07b48c53b35ec7ef2eaa15f0d8ca9c5
Bennette321321 <6631321321@student.chula.ac.th>
2025-08-10 15:15:30 +0700

    fix: again again

--- api/index.ts
+++ api/index.ts
@@ -6,13 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD:src/index.ts
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.json({ message: 'Hello World Again!' });
-=======
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Backend is running on Vercel!' });
->>>>>>> 55c026499ce302797e425fb40d406f59ce30621f:api/index.ts
 });
 
 app.get('/api/hello', (req: Request, res: Response) => {
